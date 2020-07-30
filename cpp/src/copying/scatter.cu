mythrocks/cudf

[WIP] [struct] Initial commit
A working struct test, verifying that struct composition works.

cpp/src/copying/scatter.cu
@@ -25,6 +25,7 @@
 #include <cudf/detail/stream_compaction.hpp>
 #include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/lists/list_view.cuh>
+#include <cudf/structs/structs_view.hpp>
 #include <cudf/stream_compaction.hpp>
 #include <cudf/strings/detail/scatter.cuh>
 #include <cudf/strings/string_view.cuh>
@@ -185,6 +186,19 @@ struct column_scalar_scatterer_impl<list_view, MapIterator> {
                                      cudaStream_t stream) const
   {
     CUDF_FAIL("scatter scalar to list_view not implemented");
+  }
+};
+
+template <typename MapIterator>
+struct column_scalar_scatterer_impl<struct_view, MapIterator> {
+  std::unique_ptr<column> operator()(std::unique_ptr<scalar> const& source,
+                                     MapIterator scatter_iter,
+                                     size_type scatter_rows,
+                                     column_view const& target,
+                                     rmm::mr::device_memory_resource* mr,
+                                     cudaStream_t stream) const
+  {
+    CUDF_FAIL("scatter scalar to struct_view not implemented");
   }
 };
 

cpp/src/reductions/simple.cuh
@@ -21,6 +21,7 @@
 #include <cudf/scalar/scalar_factories.hpp>
 #include <cudf/utilities/traits.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
+#include "cudf/structs/structs_view.hpp"
 
 namespace cudf {
 namespace reduction {
@@ -80,7 +81,8 @@ struct result_type_dispatcher {
            (std::is_arithmetic<ResultType>::value ||
             std::is_same<Op, cudf::reduction::op::min>::value ||
             std::is_same<Op, cudf::reduction::op::max>::value) &&
-           !std::is_same<ResultType, cudf::list_view>::value;
+           !std::is_same<ResultType, cudf::list_view>::value &&
+           !std::is_same<ResultType, cudf::struct_view>::value;
   }
 
  public:
@@ -117,7 +119,8 @@ struct element_type_dispatcher {
               !(std::is_same<Op, cudf::reduction::op::min>::value ||
                 std::is_same<Op, cudf::reduction::op::max>::value))
              // disable for list views
-             || std::is_same<ElementType, cudf::list_view>::value);
+             || std::is_same<ElementType, cudf::list_view>::value
+             || std::is_same<ElementType, cudf::struct_view>::value);
   }
 
  public:

ROCm-DS/hipDF

cpp/src/copying/scatter.cu
@@ -25,6 +25,7 @@
 #include <cudf/detail/stream_compaction.hpp>
 #include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/lists/list_view.cuh>
+#include <cudf/structs/struct_view.hpp>
 #include <cudf/stream_compaction.hpp>
 #include <cudf/strings/detail/scatter.cuh>
 #include <cudf/strings/string_view.cuh>
@@ -185,6 +186,19 @@ struct column_scalar_scatterer_impl<list_view, MapIterator> {
                                      cudaStream_t stream) const
   {
     CUDF_FAIL("scatter scalar to list_view not implemented");
+  }
+};
+
+template <typename MapIterator>
+struct column_scalar_scatterer_impl<struct_view, MapIterator> {
+  std::unique_ptr<column> operator()(std::unique_ptr<scalar> const& source,
+                                     MapIterator scatter_iter,
+                                     size_type scatter_rows,
+                                     column_view const& target,
+                                     rmm::mr::device_memory_resource* mr,
+                                     cudaStream_t stream) const
+  {
+    CUDF_FAIL("scatter scalar to struct_view not implemented");
   }
 };
 

cpp/src/reductions/simple.cuh
@@ -21,6 +21,7 @@
 #include <cudf/scalar/scalar_factories.hpp>
 #include <cudf/utilities/traits.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
+#include "cudf/structs/struct_view.hpp"
 
 namespace cudf {
 namespace reduction {
@@ -80,7 +81,8 @@ struct result_type_dispatcher {
            (std::is_arithmetic<ResultType>::value ||
             std::is_same<Op, cudf::reduction::op::min>::value ||
             std::is_same<Op, cudf::reduction::op::max>::value) &&
-           !std::is_same<ResultType, cudf::list_view>::value;
+           !std::is_same<ResultType, cudf::list_view>::value &&
+           !std::is_same<ResultType, cudf::struct_view>::value;
   }
 
  public:
@@ -117,7 +119,8 @@ struct element_type_dispatcher {
               !(std::is_same<Op, cudf::reduction::op::min>::value ||
                 std::is_same<Op, cudf::reduction::op::max>::value))
              // disable for list views
-             || std::is_same<ElementType, cudf::list_view>::value);
+             || std::is_same<ElementType, cudf::list_view>::value
+             || std::is_same<ElementType, cudf::struct_view>::value);
   }
 
  public: