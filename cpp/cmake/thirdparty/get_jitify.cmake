951cf8abe26e700281b6de13dbb5232941405a1d
Philipp Samfass <philipp.samfass@amd.com>
2023-09-12 07:25:34 +0000

    refactoring (libhipdf): use HIP port of jitify in buil in build

--- cpp/cmake/thirdparty/get_jitify.cmake
+++ cpp/cmake/thirdparty/get_jitify.cmake
@@ -18,8 +18,8 @@
 function(find_and_configure_jitify)
   rapids_cpm_find(
     jitify 2.0.0
-    GIT_REPOSITORY https://github.com/rapidsai/jitify.git
-    GIT_TAG jitify2
+    GIT_REPOSITORY https://$ENV{GITHUB_USER}:$ENV{GITHUB_PASS}@github.com/AMD-AI/jitify
+    GIT_TAG dev
     GIT_SHALLOW TRUE
     DOWNLOAD_ONLY TRUE
   )
