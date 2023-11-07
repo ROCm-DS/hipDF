c70feee37cbf66371516d7c58c951c572f1cdcbe
Philipp Samfass <philipp.samfass@amd.com>
2023-11-07 01:17:28 -0800

    fix: the default cast type for returning null_mask must be changed from uint32_t to bitmask_type for the HIP port

--- cpp/src/io/utilities/column_buffer.hpp
+++ cpp/src/io/utilities/column_buffer.hpp
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 /**
  * @file column_buffer.hpp
  * @brief cuDF-IO Column-backing buffer utilities
@@ -141,7 +163,7 @@ class column_buffer_base {
 
   void set_null_mask(rmm::device_buffer&& mask) { _null_mask = std::move(mask); }
 
-  template <typename T = uint32_t>
+  template <typename T = bitmask_type>
   auto null_mask()
   {
     return static_cast<T*>(_null_mask.data());
