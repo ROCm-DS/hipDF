10c216112f3f99e3b9daf1627deb71c418a52449
Philipp Samfass <philipp.samfass@amd.com>
2023-12-21 02:49:54 -0800

    fix: "hipify" JIT-compilation flag "-arch=sm" to "--offload-arch=gfx.",  which makes jitify find the current architecture to JIT-compile the code for. For CUDA backend compatibility, we need to revisit this piece of code and use "-arch=sm_." for CUDA's jitify.

--- cpp/src/rolling/detail/rolling.cuh
+++ cpp/src/rolling/detail/rolling.cuh
@@ -1304,8 +1304,8 @@ std::unique_ptr<column> rolling_window_udf(column_view const& input,
 
   cudf::jit::get_program_cache(*rolling_jit_kernel_cu_jit)
     .get_kernel(
-      kernel_name, {}, {{"rolling/jit/operation-udf.hpp", cuda_source}}, {"-arch=sm_."})  //
-    ->configure_1d_max_occupancy(0, 0, 0, stream.value())                                 //
+      kernel_name, {}, {{"rolling/jit/operation-udf.hpp", cuda_source}}, {"--offload-arch=gfx."}) //: TODO : HIP/AMD : On CUDA, we need to change this flag to -arch=sm_.
+    ->configure_1d_max_occupancy(0, 0, 0, stream.value())                                         // for CUDA's jitify.
     ->launch(input.size(),
              cudf::jit::get_data_ptr(input),
              input.null_mask(),
