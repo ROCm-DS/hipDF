8e5b82a015f32be8bf49e69dd5b530f25206154d
Philipp Samfass <philipp.samfass@amd.com>
2024-02-14 07:32:25 -0800

    tmpfix (python unit tests): disable some test files where unit tests are hanging indefinitely (due to using presently unsupported APIs) since this would prevent the CI from running through all tests. Please re-enable these tests whenever the underlying APIs have been ported.

--- python/cudf/cudf/tests/conftest.py
+++ python/cudf/cudf/tests/conftest.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2019-2024, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import itertools
 import os
 import pathlib
@@ -176,3 +198,16 @@ def pytest_runtest_makereport(item, call):
     # Set a report attribute for each phase of a call, which can
     # be "setup", "call", "teardown"
     setattr(item, "report", {rep.when: rep})
+
+
+#TODO(HIP): we are disabling these tests as they hang currently. Once support for these APIs are added,
+# these tests should be enabled again
+disabled_test_files = ["test_orc.py", "test_parquet.py", "test_s3.py", "test_avro_reader_fastavro_integration.py"]
+
+def pytest_collection_modifyitems(items):
+    exclude = []
+    for item in items:
+        if item.fspath.basename in disabled_test_files:
+            exclude.append(item)
+    for item in exclude:
+        items.remove(item)
