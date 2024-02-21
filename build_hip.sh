ff13994402e50839d6a228618379e0005356285c
Philipp Samfass <philipp.samfass@amd.com>
2024-02-21 06:14:24 -0800

    refactor: further refactoring of open TODO comments for HIP/AMD

--- build_hip.sh
+++ build_hip.sh
@@ -358,7 +358,7 @@ if buildAll || hasArg hipdf; then
     SKBUILD_CONFIGURE_OPTIONS="-DCMAKE_PREFIX_PATH=${INSTALL_PREFIX} -DCMAKE_LIBRARY_PATH=${LIBHIPDF_BUILD_DIR} -DCMAKE_HIP_ARCHITECTURES=${HIPDF_CMAKE_HIP_ARCHITECTURES} ${EXTRA_CMAKE_ARGS}" \
         SKBUILD_BUILD_OPTIONS="-j${PARALLEL_LEVEL:-1}" \
         python setup.py bdist_wheel
-        # python -m pip install --no-build-isolation --no-deps . #: TODO: HIP/AMD: results in a cmake Cache issue, the binary wheel is preferred in any case
+        # python -m pip install --no-build-isolation --no-deps . #: TODO(HIP/AMD): results in a cmake Cache issue, the binary wheel is preferred in any case
 	echo "cuDF package wheel (install via pip): $(ls ${REPODIR}/python/cudf/dist/*whl)"
 fi
 
