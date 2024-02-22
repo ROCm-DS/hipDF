3f3e1e0e0e629a475c31907a978daf0c5e8b4184
Philipp Samfass <philipp.samfass@amd.com>
2024-02-22 00:37:46 -0800

    fix(io, orc, encoding): use manual 64bit implementation of funnelshift only on AMD platform, otherwise fall back to CUDA intrinsic

--- cpp/src/io/orc/stripe_enc.cu
+++ cpp/src/io/orc/stripe_enc.cu
@@ -225,7 +225,11 @@ static __device__ uint32_t ByteRLE(
       repeat_run  = 0;
       while (literal_run < maxvals) {
         bitmask_type next = s->u.byterle.rpt_map[(literal_run >> LOG2_WARPSIZE) + 1];
+#ifdef __HIP_PLATFORM_AMD__
         bitmask_type mask = rpt_map & cudf::detail::__m_funnelshift_r(rpt_map, next, 1);
+#else
+        bitmask_type mask = rpt_map & __funnelshift_r(rpt_map, next, 1);
+#endif
         if (mask) {
           uint32_t literal_run_ofs = __FFS(mask) - 1;
           literal_run += literal_run_ofs;
