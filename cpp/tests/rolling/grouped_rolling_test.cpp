debb482cd7dfa00fa20d182be291d12727a5b020
Philipp Samfass <philipp.samfass@amd.com>
2024-02-28 01:06:04 -0800

    fix (grouped_rolling_test): fix compilation error

--- cpp/tests/rolling/grouped_rolling_test.cpp
+++ cpp/tests/rolling/grouped_rolling_test.cpp
@@ -137,7 +137,7 @@ const std::string ptx_func{
   }
   )***"};
 
-const std::string amd_llvm_ir_func{
+std::string amd_llvm_ir_func{
     R"'''(
 source_filename = "device_rolling.hip"
 target datalayout = "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7"
