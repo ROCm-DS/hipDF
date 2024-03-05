e4b464310b48b950415bdf4834d7e5c4a955436a
domcharrier <docharri@amd.com>
2024-03-05 06:11:57 -0800

    Revert "__init__.py: comment on where to place pose_as_cuda()"
    This reverts commit 7a2105df23ad68af366fb9ba67d1dd671a4fae84.

--- python/cudf/cudf/__init__.py
+++ python/cudf/cudf/__init__.py
@@ -42,7 +42,6 @@ _setup_numba()
 validate_setup()
 
 try:
-    # TODO(HIP/AMD): We need to investigate the above numba setup and validation and then potentially comment it or move this import before it.
     from numba import hip
     hip.pose_as_cuda()
 except ImportError:
