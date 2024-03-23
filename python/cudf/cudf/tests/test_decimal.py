0bcb4ad9bc187c463752d1dfaa45b62f672a0f75
Philipp Samfass <philipp.samfass@amd.com>
2024-03-23 05:24:24 -0700

    fix/feat(python unit tests, test_decimal.py): Fix failing test_typecast_from_int_to_decimal:
    On AMD backend, cast from negative float to unsigned integer
    results in large unsigned integers (on NVIDIA they happen to be casted to 0).
    These large integers then do not fit into the Decimal64Dtypes used in this test.
    This is UB so we take the abs value here if an unsigned integer is used as
     from_dtype. See issue 106.

--- python/cudf/cudf/tests/test_decimal.py
+++ python/cudf/cudf/tests/test_decimal.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2021-2024, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import decimal
 from decimal import Decimal
 
@@ -139,6 +161,14 @@ def test_typecast_from_float_to_decimal(request, data, from_dtype, to_dtype):
     [Decimal64Dtype(9, 3), Decimal64Dtype(11, 4), Decimal64Dtype(18, 9)],
 )
 def test_typecast_from_int_to_decimal(data, from_dtype, to_dtype):
+    # NOTE(HIP): On AMD backend, cast from negative float to unsigned integer
+    # results in large unsigned integers (on NVIDIA they happen to be casted to 0).
+    # These large integers then do not fit into the Decimal64Dtypes used in this test.
+    # This is UB so we take the abs value here if an unsigned integer is used as
+    # from_dtype. See internal issue 106.
+    if(not(from_dtype in SIGNED_TYPES)):
+        data = data.abs()
+    
     got = data.astype(from_dtype)
 
     pa_arr = (
