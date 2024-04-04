8ca2ab6d60200b6e6f028a1962cb2a22e24292d0
Philipp Samfass <philipp.samfass@amd.com>
2024-04-04 07:23:44 -0700

    tmpfix(unit tests, python): expect test_timedelta_series_mod_with_cudf_scalar_zero and test_timedelta_series_mod_with_scalar_zero to pass on HIP/AMD.
    Background:
    Both tests eventually call the operation x % 0 in the libhipdf
    backend, which is UB.
    On CUDA side, the integer -1 is returned as a result which
    makes the test expectedly fail, as the resulting dataframe
    does not match the one from Pandas.
    Pandas-compatible behavior is desired though and it is
    what we observe on HIP/AMD backend.
    We therefore no longer mark the test as xfail for HIP/AMD.
    CAUTION: The fundamental behaviour of 'x % 0' remains undefined,
    so for future compiler releases
    or Pandas versions, this test may need to be changed again.

--- python/cudf/cudf/tests/test_timedelta.py
+++ python/cudf/cudf/tests/test_timedelta.py
@@ -481,21 +481,21 @@ def test_timedelta_series_ops_with_scalars(data, other_scalars, dtype, op):
     assert_eq(expected, actual)
 
 
+# TODO(HIP/AMD):
+# The operation x % 0 is undefined behavior.
+# It is used by libhipdf (PyMod) in this test case for reverse=true.
+# On CUDA, x % 0 returns -1, so the test fails expectedly (that is why it
+# originally is marked as xfail).
+# With HIP/AMD, x % 0 == x holds which matches Pandas' behavior.
+# We therefore no longer mark the test as xfail for HIP/AMD.
+# CAUTION: The fundamental behaviour of 'x % 0' remains undefined,
+# so for future compiler releases
+# or Pandas versions, this test may need to be changed again.
 @pytest.mark.parametrize(
     "reverse",
     [
         False,
-        pytest.param(
-            True,
-            marks=pytest.mark.xfail(
-                strict=True,
-                reason=(
-                    "timedelta modulo by zero is dubiously defined in "
-                    "both pandas and cuDF "
-                    "(see https://github.com/rapidsai/cudf/issues/5938)"
-                ),
-            ),
-        ),
+        True
     ],
 )
 def test_timedelta_series_mod_with_scalar_zero(reverse):
@@ -599,21 +599,21 @@ def test_timedelta_series_ops_with_cudf_scalars(data, cpu_scalar, dtype, op):
     assert_eq(expected, actual)
 
 
+# TODO(HIP/AMD):
+# The operation x % 0 is undefined behavior.
+# It is used by libhipdf (PyMod) in this test case for reverse=true.
+# On CUDA, x % 0 returns -1, so the test fails expectedly (that is why it
+# originally is marked as xfail).
+# With HIP/AMD, x % 0 == x holds which matches Pandas' behavior.
+# We therefore no longer mark the test as xfail for HIP/AMD.
+# CAUTION: The fundamental behaviour of 'x % 0' remains undefined,
+# so for future compiler releases
+# or Pandas versions, this test may need to be changed again.
 @pytest.mark.parametrize(
     "reverse",
     [
         False,
-        pytest.param(
-            True,
-            marks=pytest.mark.xfail(
-                strict=True,
-                reason=(
-                    "timedelta modulo by zero is dubiously defined in "
-                    "both pandas and cuDF "
-                    "(see https://github.com/rapidsai/cudf/issues/5938)"
-                ),
-            ),
-        ),
+        True
     ],
 )
 def test_timedelta_series_mod_with_cudf_scalar_zero(reverse):
