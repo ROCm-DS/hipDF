ab2f8803e887d2ee83b14982a25dddfa75bdeba4
Mohammad Norouzi <114993067+monoatamd@users.noreply.github.com>
2024-07-04 15:51:08 +0200

    Disable nvtx

--- java/src/main/native/src/check_nvcomp_output_sizes.cu
+++ java/src/main/native/src/check_nvcomp_output_sizes.cu
@@ -13,11 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "check_nvcomp_output_sizes.hpp"
 
 #include <cudf/utilities/error.hpp>
 
+#ifndef NVTX_DISABLE
 #include <nvtx3/nvtx3.hpp>
+#endif
 #include <thrust/device_ptr.h>
 #include <thrust/equal.h>
 
@@ -41,7 +66,9 @@ bool check_nvcomp_output_sizes(std::size_t const* dev_uncompressed_sizes,
                                std::size_t num_chunks,
                                rmm::cuda_stream_view stream)
 {
+#ifndef NVTX_DISABLE
   NVTX3_FUNC_RANGE_IN(java_domain);
+#endif
   return thrust::equal(rmm::exec_policy(stream),
                        dev_uncompressed_sizes,
                        dev_uncompressed_sizes + num_chunks,
