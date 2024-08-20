2ba77482d7cc13ecd2494e3c3a1a7dee27afa070
domcharrier <docharri@amd.com>
2024-08-20 15:13:32 +0000

    fetch_rapids.cmake: reverse hipify

--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -11,7 +11,7 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 # =============================================================================
-if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/HIPDF_RAPIDS.cmake)
+if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
   if (DEFINED ENV{RAPIDS_CMAKE_BRANCH})
     set(RAPIDS_CMAKE_BRANCH "$ENV{RAPIDS_CMAKE_BRANCH}")
   else()
@@ -21,18 +21,18 @@ if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/HIPDF_RAPIDS.cmake)
   # TODO(HIP/AMD): once rapids-cmake is publically available for HIP, we can remove the authentication needed here
   set(URL "https://$ENV{GITHUB_USER}:$ENV{GITHUB_PASS}@raw.githubusercontent.com/AMD-AI/rapids-cmake/${RAPIDS_CMAKE_BRANCH}/RAPIDS.cmake")
   file(DOWNLOAD ${URL}
-       ${CMAKE_CURRENT_BINARY_DIR}/HIPDF_RAPIDS.cmake
+       ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake
        STATUS DOWNLOAD_STATUS
   )
   list(GET DOWNLOAD_STATUS 0 STATUS_CODE)
   list(GET DOWNLOAD_STATUS 1 ERROR_MESSAGE)
 
   if(${STATUS_CODE} EQUAL 0)
-    message(STATUS "Downloaded 'HIPDF_RAPIDS.cmake' successfully!")
+    message(STATUS "Downloaded 'CUDF_RAPIDS.cmake' successfully!")
   else()
-    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/HIPDF_RAPIDS.cmake)
-    # for debuging: message(FATAL_ERROR "Failed to download 'HIPDF_RAPIDS.cmake'. URL: ${URL}, Reason: ${ERROR_MESSAGE}")
-    message(FATAL_ERROR "Failed to download 'HIPDF_RAPIDS.cmake'. Reason: ${ERROR_MESSAGE}")
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
+    # for debuging: message(FATAL_ERROR "Failed to download 'CUDF_RAPIDS.cmake'. URL: ${URL}, Reason: ${ERROR_MESSAGE}")
+    message(FATAL_ERROR "Failed to download 'CUDF_RAPIDS.cmake'. Reason: ${ERROR_MESSAGE}")
   endif()
 endif()
-include(${CMAKE_CURRENT_BINARY_DIR}/HIPDF_RAPIDS.cmake)
+include(${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
