0ddd0b50fca96eba0a213cc59abe5c48d9aa5242
domcharrier <docharri@amd.com>
2024-08-21 13:21:02 +0000

    wip(cpp/examples): reverse hipify

--- cpp/examples/strings/libcudf_apis.cpp
+++ cpp/examples/strings/libcudf_apis.cpp
@@ -55,7 +55,7 @@ std::unique_ptr<cudf::column> redact_strings(cudf::column_view const& names,
 
   auto result = cudf::strings::concatenate(last_initial_first, std::string(" "));
 
-  hipStreamSynchronize(0);
+  cudaStreamSynchronize(0);
 
   nvtxRangePop();
   return result;
