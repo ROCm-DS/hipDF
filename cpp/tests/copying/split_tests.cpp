3073d7cdd0be6888b595da136d87fcb0bda2a4b2
domcharrier <docharri@amd.com>
2024-08-21 13:18:10 +0000

    wip(cpp/tests): add static_cast for cudaMemcpyAsync

--- cpp/tests/copying/split_tests.cpp
+++ cpp/tests/copying/split_tests.cpp
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <tests/copying/slice_tests.cuh>
 
 #include <cudf_test/base_fixture.hpp>
@@ -1404,11 +1426,11 @@ std::vector<cudf::packed_table> do_chunked_pack(cudf::table_view const& input)
   std::size_t final_buff_offset = 0;
   while (chunked_pack->has_next()) {
     auto bytes_copied = chunked_pack->next(bounce_buff_span);
-    cudaMemcpyAsync((uint8_t*)final_buff.data() + final_buff_offset,
+    static_cast<void>(cudaMemcpyAsync((uint8_t*)final_buff.data() + final_buff_offset,
                     bounce_buff.data(),
                     bytes_copied,
                     cudaMemcpyDefault,
-                    cudf::get_default_stream());
+                    cudf::get_default_stream()));
     final_buff_offset += bytes_copied;
   }
 
