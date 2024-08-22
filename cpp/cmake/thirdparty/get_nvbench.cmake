6d94457fdc73acbff08838f4aa161e27f766fe49
domcharrier <docharri@amd.com>
2024-08-22 14:30:12 +0000

    wip(cpp): cmake files: + modifications lic
    Prepend modification license to all files changed since initial commit
    (f90b8d41da).

--- cpp/cmake/thirdparty/get_nvbench.cmake
+++ cpp/cmake/thirdparty/get_nvbench.cmake
@@ -11,10 +11,9 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 # =============================================================================
-
 # MIT License
 #
-# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+# Modifications Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -33,6 +32,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# =============================================================================
 
 # This function finds nvbench and applies any needed patches.
 function(find_and_configure_hipbench)
