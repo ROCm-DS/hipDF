ROCm-DS/hipDF

commit 3573180d8f7e5daab4760dcdfe2ddd29272996da

Rework cudf::strings::detail::copy_range for offsetalator (#15010)
This reworks the `cudf::strings::detail::copy_range()` function to use the offsetalator instead of accessing the output offsets directly. Also refactored the code to remove the unnecessary template arguments. And added a benchmark to ensure these changes did not cause a performance impact.

Most of the code in `cpp/include/cudf/strings/detail/copy_range.cuh` was rewritten and moved to `cpp/src/strings/copying/copy_range.cu`.

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - Robert Maynard (https://github.com/robertmaynard)
  - Vukasin Milovanovic (https://github.com/vuule)

URL: https://github.com/rapidsai/cudf/pull/15010

cpp/src/strings/copying/copy_range.cu
@@ -1,0 +1,143 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/column/column_device_view.cuh>
+#include <cudf/detail/offsets_iterator_factory.cuh>
+#include <cudf/detail/valid_if.cuh>
+#include <cudf/strings/detail/copy_range.hpp>
+#include <cudf/strings/detail/strings_children.cuh>
+#include <cudf/strings/string_view.cuh>
+#include <cudf/strings/strings_column_view.hpp>
+#include <cudf/types.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/exec_policy.hpp>
+
+#include <thrust/for_each.h>
+#include <thrust/iterator/counting_iterator.h>
+
+namespace cudf {
+namespace strings {
+namespace detail {
+namespace {
+struct compute_element_size {
+  column_device_view d_source;
+  column_device_view d_target;
+  size_type source_begin;
+  size_type target_begin;
+  size_type target_end;
+  bool source_has_nulls;
+  bool target_has_nulls;
+
+  __device__ cudf::size_type operator()(cudf::size_type idx)
+  {
+    if (idx >= target_begin && idx < target_end) {
+      auto const str_idx = source_begin + (idx - target_begin);
+      return source_has_nulls && d_source.is_null_nocheck(str_idx)
+               ? 0
+               : d_source.element<string_view>(str_idx).size_bytes();
+    } else {
+      return target_has_nulls && d_target.is_null_nocheck(idx)
+               ? 0
+               : d_target.element<string_view>(idx).size_bytes();
+    }
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<column> copy_range(strings_column_view const& source,
+                                   strings_column_view const& target,
+                                   size_type source_begin,
+                                   size_type source_end,
+                                   size_type target_begin,
+                                   rmm::cuda_stream_view stream,
+                                   rmm::mr::device_memory_resource* mr)
+{
+  auto target_end = target_begin + (source_end - source_begin);
+  CUDF_EXPECTS(
+    (target_begin >= 0) && (target_begin < target.size()) && (target_end <= target.size()),
+    "Range is out of bounds.",
+    std::invalid_argument);
+
+  if (target_end == target_begin) { return std::make_unique<column>(target.parent(), stream, mr); }
+  auto source_device_view = column_device_view::create(source.parent(), stream);
+  auto d_source           = *source_device_view;
+  auto target_device_view = column_device_view::create(target.parent(), stream);
+  auto d_target           = *target_device_view;
+
+  // create null mask
+  auto [null_mask, null_count] = [&] {
+    if (!target.parent().nullable() && !source.parent().nullable()) {
+      return std::pair(rmm::device_buffer{}, 0);
+    }
+    return cudf::detail::valid_if(
+      thrust::make_counting_iterator<size_type>(0),
+      thrust::make_counting_iterator<size_type>(target.size()),
+      [d_source, d_target, source_begin, target_begin, target_end] __device__(size_type idx) {
+        return (idx >= target_begin && idx < target_end)
+                 ? d_source.is_valid(source_begin + (idx - target_begin))
+                 : d_target.is_valid(idx);
+      },
+      stream,
+      mr);
+  }();
+
+  auto [check_source, check_target] = [target, null_count = null_count] {
+    // check validities for both source & target
+    if (target.has_nulls()) { return std::make_pair(true, true); }
+    // check validities for source only
+    if (null_count > 0) { return std::make_pair(true, false); }
+    // no need to check validities
+    return std::make_pair(false, false);
+  }();
+
+  // create offsets
+  auto sizes_begin = cudf::detail::make_counting_transform_iterator(
+    0,
+    compute_element_size{
+      d_source, d_target, source_begin, target_begin, target_end, check_source, check_target});
+  auto [offsets_column, chars_bytes] = cudf::strings::detail::make_offsets_child_column(
+    sizes_begin, sizes_begin + target.size(), stream, mr);
+  auto d_offsets = cudf::detail::offsetalator_factory::make_input_iterator(offsets_column->view());
+
+  // create chars
+  auto chars_data = rmm::device_uvector<char>(chars_bytes, stream, mr);
+  auto d_chars    = chars_data.data();
+  thrust::for_each(
+    rmm::exec_policy_nosync(stream),
+    thrust::make_counting_iterator(0),
+    thrust::make_counting_iterator(target.size()),
+    [d_source, d_target, source_begin, target_begin, target_end, d_offsets, d_chars] __device__(
+      size_type idx) {
+      if (d_offsets[idx + 1] - d_offsets[idx] > 0) {
+        const auto source = (idx >= target_begin && idx < target_end)
+                              ? d_source.element<string_view>(source_begin + (idx - target_begin))
+                              : d_target.element<string_view>(idx);
+        memcpy(d_chars + d_offsets[idx], source.data(), source.size_bytes());
+      }
+    });
+
+  return make_strings_column(target.size(),
+                             std::move(offsets_column),
+                             chars_data.release(),
+                             null_count,
+                             std::move(null_mask));
+}
+
+}  // namespace detail
+}  // namespace strings
+}  // namespace cudf

cpp/src/copying/copy_range.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@
 #include <cudf/dictionary/detail/update_keys.hpp>
 #include <cudf/dictionary/dictionary_column_view.hpp>
 #include <cudf/dictionary/dictionary_factories.hpp>
-#include <cudf/strings/detail/copy_range.cuh>
+#include <cudf/strings/detail/copy_range.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 #include <cudf/types.hpp>
 #include <cudf/utilities/default_stream.hpp>
@@ -130,29 +130,8 @@ std::unique_ptr<cudf::column> out_of_place_copy_range_dispatch::operator()<cudf:
   rmm::cuda_stream_view stream,
   rmm::mr::device_memory_resource* mr)
 {
-  auto target_end           = target_begin + (source_end - source_begin);
-  auto p_source_device_view = cudf::column_device_view::create(source, stream);
-  if (source.has_nulls()) {
-    return cudf::strings::detail::copy_range(
-      cudf::detail::make_null_replacement_iterator<cudf::string_view>(*p_source_device_view,
-                                                                      cudf::string_view()) +
-        source_begin,
-      cudf::detail::make_validity_iterator(*p_source_device_view) + source_begin,
-      cudf::strings_column_view(target),
-      target_begin,
-      target_end,
-      stream,
-      mr);
-  } else {
-    return cudf::strings::detail::copy_range(
-      p_source_device_view->begin<cudf::string_view>() + source_begin,
-      thrust::make_constant_iterator(true),
-      cudf::strings_column_view(target),
-      target_begin,
-      target_end,
-      stream,
-      mr);
-  }
+  return cudf::strings::detail::copy_range(
+    source, target, source_begin, source_end, target_begin, stream, mr);
 }
 
 template <>

cpp/CMakeLists.txt
@@ -570,6 +570,7 @@ add_library(
   src/strings/convert/convert_lists.cu
   src/strings/copying/concatenate.cu
   src/strings/copying/copying.cu
+  src/strings/copying/copy_range.cu
   src/strings/copying/shift.cu
   src/strings/count_matches.cu
   src/strings/extract/extract.cu

cpp/benchmarks/CMakeLists.txt
@@ -312,6 +312,7 @@ ConfigureNVBench(
   string/char_types.cpp
   string/contains.cpp
   string/copy_if_else.cpp
+  string/copy_range.cpp
   string/count.cpp
   string/extract.cpp
   string/gather.cpp

cpp/benchmarks/string/copy_range.cpp
@@ -1,0 +1,60 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <benchmarks/common/generate_input.hpp>
+
+#include <cudf/copying.hpp>
+#include <cudf/strings/strings_column_view.hpp>
+#include <cudf/utilities/default_stream.hpp>
+
+#include <nvbench/nvbench.cuh>
+
+static void bench_copy_range(nvbench::state& state)
+{
+  auto const num_rows  = static_cast<cudf::size_type>(state.get_int64("num_rows"));
+  auto const row_width = static_cast<cudf::size_type>(state.get_int64("row_width"));
+
+  if (static_cast<std::size_t>(num_rows) * static_cast<std::size_t>(row_width) >=
+      static_cast<std::size_t>(std::numeric_limits<cudf::size_type>::max())) {
+    state.skip("Skip benchmarks greater than size_type limit");
+  }
+
+  data_profile const table_profile =
+    data_profile_builder()
+      .distribution(cudf::type_id::STRING, distribution_id::NORMAL, 0, row_width)
+      .no_validity();
+  auto const source_tables = create_random_table(
+    {cudf::type_id::STRING, cudf::type_id::STRING}, row_count{num_rows}, table_profile);
+
+  auto const start  = num_rows / 4;
+  auto const end    = (num_rows * 3) / 4;
+  auto const source = source_tables->view().column(0);
+  auto const target = source_tables->view().column(1);
+
+  state.set_cuda_stream(nvbench::make_cuda_stream_view(cudf::get_default_stream().value()));
+  auto chars_size = cudf::strings_column_view(target).chars_size(cudf::get_default_stream());
+  state.add_global_memory_reads<nvbench::int8_t>(chars_size);   // all bytes are read;
+  state.add_global_memory_writes<nvbench::int8_t>(chars_size);  // both columns are similar size
+
+  state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    [[maybe_unused]] auto result = cudf::copy_range(source, target, start, end, start / 2);
+  });
+}
+
+NVBENCH_BENCH(bench_copy_range)
+  .set_name("copy_range")
+  .add_int64_axis("row_width", {32, 64, 128, 256, 512, 1024, 2048, 4096})
+  .add_int64_axis("num_rows", {4096, 32768, 262144, 2097152, 16777216});

cpp/include/cudf/strings/detail/copy_range.cuh
@@ -1,216 +1,0 @@
-/*
- * Copyright (c) 2019-2024, NVIDIA CORPORATION.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#pragma once
-
-#include <cudf/column/column_device_view.cuh>
-#include <cudf/detail/valid_if.cuh>
-#include <cudf/strings/detail/strings_children.cuh>
-#include <cudf/strings/string_view.cuh>
-#include <cudf/strings/strings_column_view.hpp>
-#include <cudf/types.hpp>
-
-#include <rmm/cuda_stream_view.hpp>
-#include <rmm/exec_policy.hpp>
-
-#include <thrust/device_ptr.h>
-#include <thrust/for_each.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/transform_iterator.h>
-
-namespace {
-template <bool source_has_nulls,
-          bool target_has_nulls,
-          typename SourceValueIterator,
-          typename SourceValidityIterator>
-struct compute_element_size {
-  SourceValueIterator source_value_begin;
-  SourceValidityIterator source_validity_begin;
-  cudf::column_device_view d_target;
-  cudf::size_type target_begin;
-  cudf::size_type target_end;
-
-  __device__ cudf::size_type operator()(cudf::size_type idx)
-  {
-    if (idx >= target_begin && idx < target_end) {
-      if (source_has_nulls) {
-        return *(source_validity_begin + (idx - target_begin))
-                 ? (*(source_value_begin + (idx - target_begin))).size_bytes()
-                 : 0;
-      } else {
-        return (*(source_value_begin + (idx - target_begin))).size_bytes();
-      }
-    } else {
-      if (target_has_nulls) {
-        return d_target.is_valid_nocheck(idx)
-                 ? d_target.element<cudf::string_view>(idx).size_bytes()
-                 : 0;
-      } else {
-        return d_target.element<cudf::string_view>(idx).size_bytes();
-      }
-    }
-  }
-};
-
-}  // namespace
-
-namespace cudf {
-namespace strings {
-namespace detail {
-/**
- * @brief Internal API to copy a range of string elements out-of-place from
- * source iterators to a target column.
- *
- * Creates a new column as if an in-place copy was performed into @p target.
- * The elements indicated by the indices [@p target_begin, @p target_end) were
- * replaced with the elements retrieved from source iterators;
- * *(@p source_value_begin + idx) if *(@p source_validity_begin + idx) is true,
- * invalidate otherwise (where idx = [0, @p target_end - @p target_begin)).
- * Elements outside the range are copied from @p target into the new target
- * column to return.
- *
- * @throws cudf::logic_error for invalid range (if @p target_begin < 0,
- * target_begin >= @p target.size(), or @p target_end > @p target.size()).
- *
- * @tparam SourceValueIterator Iterator for retrieving source values
- * @tparam SourceValidityIterator Iterator for retrieving source validities
- * @param source_value_begin Start of source value iterator
- * @param source_validity_begin Start of source validity iterator
- * @param target The strings column to copy from outside the range.
- * @param target_begin The starting index of the target range (inclusive)
- * @param target_end The index of the last element in the target range
- * (exclusive)
- * @param stream CUDA stream used for device memory operations and kernel launches.
- * @param mr Device memory resource used to allocate the returned column's device memory.
- * @return std::unique_ptr<column> The result target column
- */
-template <typename SourceValueIterator, typename SourceValidityIterator>
-std::unique_ptr<column> copy_range(SourceValueIterator source_value_begin,
-                                   SourceValidityIterator source_validity_begin,
-                                   strings_column_view const& target,
-                                   size_type target_begin,
-                                   size_type target_end,
-                                   rmm::cuda_stream_view stream,
-                                   rmm::mr::device_memory_resource* mr)
-{
-  CUDF_EXPECTS(
-    (target_begin >= 0) && (target_begin < target.size()) && (target_end <= target.size()),
-    "Range is out of bounds.");
-
-  if (target_end == target_begin) {
-    return std::make_unique<column>(target.parent(), stream, mr);
-  } else {
-    auto p_target_device_view = column_device_view::create(target.parent(), stream);
-    auto d_target             = *p_target_device_view;
-
-    // create resulting null mask
-
-    std::pair<rmm::device_buffer, size_type> valid_mask{};
-    if (target.has_nulls()) {  // check validities for both source & target
-      valid_mask = cudf::detail::valid_if(
-        thrust::make_counting_iterator<size_type>(0),
-        thrust::make_counting_iterator<size_type>(target.size()),
-        [source_validity_begin, d_target, target_begin, target_end] __device__(size_type idx) {
-          return (idx >= target_begin && idx < target_end)
-                   ? *(source_validity_begin + (idx - target_begin))
-                   : d_target.is_valid_nocheck(idx);
-        },
-        stream,
-        mr);
-    } else {  // check validities for source only
-      valid_mask = cudf::detail::valid_if(
-        thrust::make_counting_iterator<size_type>(0),
-        thrust::make_counting_iterator<size_type>(target.size()),
-        [source_validity_begin, d_target, target_begin, target_end] __device__(size_type idx) {
-          return (idx >= target_begin && idx < target_end)
-                   ? *(source_validity_begin + (idx - target_begin))
-                   : true;
-        },
-        stream,
-        mr);
-    }
-
-    auto null_count = valid_mask.second;
-    rmm::device_buffer null_mask{0, stream, mr};
-    if (target.parent().nullable() || null_count > 0) { null_mask = std::move(valid_mask.first); }
-
-    // build offsets column
-
-    std::unique_ptr<column> p_offsets_column{nullptr};
-    size_type chars_bytes = 0;
-    if (target.has_nulls()) {  // check validities for both source & target
-      auto string_size_begin = thrust::make_transform_iterator(
-        thrust::make_counting_iterator(0),
-        compute_element_size<true, true, SourceValueIterator, SourceValidityIterator>{
-          source_value_begin, source_validity_begin, d_target, target_begin, target_end});
-
-      std::tie(p_offsets_column, chars_bytes) = cudf::detail::make_offsets_child_column(
-        string_size_begin, string_size_begin + target.size(), stream, mr);
-    } else if (null_count > 0) {  // check validities for source only
-      auto string_size_begin = thrust::make_transform_iterator(
-        thrust::make_counting_iterator(0),
-        compute_element_size<true, false, SourceValueIterator, SourceValidityIterator>{
-          source_value_begin, source_validity_begin, d_target, target_begin, target_end});
-
-      std::tie(p_offsets_column, chars_bytes) = cudf::detail::make_offsets_child_column(
-        string_size_begin, string_size_begin + target.size(), stream, mr);
-    } else {  // no need to check validities
-      auto string_size_begin = thrust::make_transform_iterator(
-        thrust::make_counting_iterator(0),
-        compute_element_size<false, false, SourceValueIterator, SourceValidityIterator>{
-          source_value_begin, source_validity_begin, d_target, target_begin, target_end});
-
-      std::tie(p_offsets_column, chars_bytes) = cudf::detail::make_offsets_child_column(
-        string_size_begin, string_size_begin + target.size(), stream, mr);
-    }
-
-    // create the chars column
-
-    auto p_offsets =
-      thrust::device_pointer_cast(p_offsets_column->view().template data<size_type>());
-    auto p_chars_column = strings::detail::create_chars_child_column(chars_bytes, stream, mr);
-
-    // copy to the chars column
-
-    auto p_chars = (p_chars_column->mutable_view()).template data<char>();
-    thrust::for_each(rmm::exec_policy(stream),
-                     thrust::make_counting_iterator(0),
-                     thrust::make_counting_iterator(target.size()),
-                     [source_value_begin,
-                      source_validity_begin,
-                      d_target,
-                      target_begin,
-                      target_end,
-                      p_offsets,
-                      p_chars] __device__(size_type idx) {
-                       if (p_offsets[idx + 1] - p_offsets[idx] > 0) {
-                         const auto source = (idx >= target_begin && idx < target_end)
-                                               ? *(source_value_begin + (idx - target_begin))
-                                               : d_target.element<string_view>(idx);
-                         memcpy(p_chars + p_offsets[idx], source.data(), source.size_bytes());
-                       }
-                     });
-
-    return make_strings_column(target.size(),
-                               std::move(p_offsets_column),
-                               std::move(p_chars_column->release().data.release()[0]),
-                               null_count,
-                               std::move(null_mask));
-  }
-}
-
-}  // namespace detail
-}  // namespace strings
-}  // namespace cudf

cpp/include/cudf/strings/detail/copy_range.hpp
@@ -1,0 +1,60 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cudf/strings/strings_column_view.hpp>
+#include <cudf/types.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+
+namespace cudf {
+namespace strings {
+namespace detail {
+
+/**
+ * @brief Internal API to copy a range of string elements out-of-place from
+ * a source column to a target column
+ *
+ * Creates a new column as if an in-place copy was performed into `target`.
+ * The elements indicated by the indices `source_begin`, `source_end`)
+ * replace with the elements in the target column starting at `target_begin`.
+ * Elements outside the range are copied from `target` into the new target
+ * column to return.
+ *
+ * @throws cudf::logic_error for invalid range (if `target_begin < 0`,
+ * or `target_begin >= target.size()`,
+ * or `target_begin + (source_end-source_begin)` > target.size()`).
+ *
+ * @param source The strings column to copy from inside the `target_begin` range
+ * @param target The strings column to copy from outside the range
+ * @param source_end The index of the first element in the source range
+ * @param source_end The index of the last element in the source range (exclusive)
+ * @param target_begin The starting index of the target range (inclusive)
+ * @param stream CUDA stream used for device memory operations and kernel launches.
+ * @param mr Device memory resource used to allocate the returned column's device memory.
+ * @return The result target column
+ */
+std::unique_ptr<column> copy_range(strings_column_view const& source,
+                                   strings_column_view const& target,
+                                   size_type source_begin,
+                                   size_type source_end,
+                                   size_type target_begin,
+                                   rmm::cuda_stream_view stream,
+                                   rmm::mr::device_memory_resource* mr);
+
+}  // namespace detail
+}  // namespace strings
+}  // namespace cudf

commit 4680978112ce7840eb097e0eef65f440db6bb62e

Fix strings::detail::copy_range when target contains nulls (#16626)
Fixes the logic in `cudf::strings::detail::copy_range` handling of nulls in the target range. The optimization check for nulls is removed simplifying the logic and making it more reliable as well. The benchmark showed no significant change in performance.
Also adds a specific gtest for this case.
Error was introduced in #15010
Closes #16618

Authors:
  - David Wendt (https://github.com/davidwendt)

Approvers:
  - Nghia Truong (https://github.com/ttnghia)
  - Bradley Dice (https://github.com/bdice)

URL: https://github.com/rapidsai/cudf/pull/16626

cpp/src/strings/copying/copy_range.cu
@@ -40,20 +40,14 @@ struct compute_element_size {
   size_type source_begin;
   size_type target_begin;
   size_type target_end;
-  bool source_has_nulls;
-  bool target_has_nulls;
 
   __device__ cudf::size_type operator()(cudf::size_type idx)
   {
     if (idx >= target_begin && idx < target_end) {
       auto const str_idx = source_begin + (idx - target_begin);
-      return source_has_nulls && d_source.is_null_nocheck(str_idx)
-               ? 0
-               : d_source.element<string_view>(str_idx).size_bytes();
+      return d_source.is_null(str_idx) ? 0 : d_source.element<string_view>(str_idx).size_bytes();
     } else {
-      return target_has_nulls && d_target.is_null_nocheck(idx)
-               ? 0
-               : d_target.element<string_view>(idx).size_bytes();
+      return d_target.is_null(idx) ? 0 : d_target.element<string_view>(idx).size_bytes();
     }
   }
 };
@@ -97,20 +91,9 @@ std::unique_ptr<column> copy_range(strings_column_view const& source,
       mr);
   }();
 
-  auto [check_source, check_target] = [target, null_count = null_count] {
-    // check validities for both source & target
-    if (target.has_nulls()) { return std::make_pair(true, true); }
-    // check validities for source only
-    if (null_count > 0) { return std::make_pair(true, false); }
-    // no need to check validities
-    return std::make_pair(false, false);
-  }();
-
   // create offsets
   auto sizes_begin = cudf::detail::make_counting_transform_iterator(
-    0,
-    compute_element_size{
-      d_source, d_target, source_begin, target_begin, target_end, check_source, check_target});
+    0, compute_element_size{d_source, d_target, source_begin, target_begin, target_end});
   auto [offsets_column, chars_bytes] = cudf::strings::detail::make_offsets_child_column(
     sizes_begin, sizes_begin + target.size(), stream, mr);
   auto d_offsets = cudf::detail::offsetalator_factory::make_input_iterator(offsets_column->view());

cpp/tests/copying/copy_range_tests.cpp
@@ -232,6 +232,16 @@ TEST_F(CopyRangeTestFixture, CopyWithNullsString)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(*p_ret, expected);
 }
 
+TEST_F(CopyRangeTestFixture, CopyWithTargetNullsString)
+{
+  auto target =
+    cudf::test::strings_column_wrapper({"a", "b", "", "d", "", "é"}, {1, 1, 0, 1, 1, 1});
+  auto source   = cudf::test::strings_column_wrapper({"A", "B", "C", "D", "E", "F"});
+  auto result   = cudf::copy_range(source, target, 1, 5, 1);
+  auto expected = cudf::test::strings_column_wrapper({"a", "B", "C", "D", "E", "é"});
+  CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(result->view(), expected);
+}
+
 TEST_F(CopyRangeTestFixture, CopyNoNullsString)
 {
   cudf::size_type size{100};