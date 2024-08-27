610b984414e475c9f524d8eba0da9f8cfc55b32d
Philipp Samfass <philipp.samfass@amd.com>
2024-08-27 07:04:32 -0700

    Revert "hotfix/war: On ROCm 6.2, two  PARQUET unit tests are failing, probably"
    This reverts commit 7a87862c75bc9db25cb7e852dc0458fa1b8e7aaf.

--- cpp/include/cudf/fixed_point/fixed_point.hpp
+++ cpp/include/cudf/fixed_point/fixed_point.hpp
@@ -339,9 +339,7 @@ class fixed_point {
    *
    * @return The underlying value of the `fixed_point` number
    */
-  // TODO(HIP/AMD): This is a hotfix to fix two failing PARQUET unit tests
-  // with ROCm 6.2, please see internal issue 143.
-  CUDF_HOST_DEVICE __attribute__((noinline)) rep value() const { return _value; }
+  CUDF_HOST_DEVICE inline rep value() const { return _value; }
 
   /**
    * @brief Method that returns the scale of the `fixed_point` number
