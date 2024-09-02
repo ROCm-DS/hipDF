3f9e14d311f09d0a6fbeea05bd9598c3c405f48c
domcharrier <docharri@amd.com>
2024-09-02 13:33:46 +0000

    cpp/**get_jitify.cmake: use public repo

--- cpp/cmake/thirdparty/get_jitify.cmake
+++ cpp/cmake/thirdparty/get_jitify.cmake
@@ -40,8 +40,8 @@
 function(find_and_configure_jitify)
   rapids_cpm_find(
     jitify 2.0.0
-    GIT_REPOSITORY https://$ENV{GITHUB_PASS}@github.com/AMD-AI/jitify
-    GIT_TAG dev
+    GIT_REPOSITORY https://github.com/ROCm/jitify
+    GIT_TAG main
     GIT_SHALLOW TRUE
     DOWNLOAD_ONLY TRUE
   )
