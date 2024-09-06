d0e0c9a625c4fd392472f7637bc004ffcdc3ddfb
Philipp Samfass <philipp.samfass@amd.com>
2024-09-06 05:54:19 -0700

    docfix(java/src/main/native/src/CudaJni.cpp): added comment/issue on why fourth parameter is necessary (in per-thread default stream mode)

--- java/src/main/native/src/CudaJni.cpp
+++ java/src/main/native/src/CudaJni.cpp
@@ -78,6 +78,7 @@ void auto_set_device(JNIEnv *env) {
 
 /** Fills all the bytes in the buffer 'buf' with 'value'. */
 void device_memset_async(JNIEnv *env, rmm::device_buffer &buf, char value) {
+  // FIXME(HIP/AMD): fourth argument required, see internal issue 153
   cudaError_t cuda_status = cudaMemsetAsync((void *)buf.data(), value, buf.size(), 0);
   jni_cuda_check(env, cuda_status);
 }
@@ -135,6 +136,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Cuda_memset(JNIEnv *env, jclass, jlon
   JNI_NULL_CHECK(env, dst, "dst memory pointer is null", );
   try {
     cudf::jni::auto_set_device(env);
+    // FIXME(HIP/AMD): fourth argument required, see internal issue 153
     CUDF_CUDA_TRY(cudaMemsetAsync((void *)dst, value, count, 0));
     CUDF_CUDA_TRY(cudaStreamSynchronize(0));
   }
@@ -146,6 +148,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Cuda_asyncMemset(JNIEnv *env, jclass,
   JNI_NULL_CHECK(env, dst, "dst memory pointer is null", );
   try {
     cudf::jni::auto_set_device(env);
+    // FIXME(HIP/AMD): fourth argument required, see internal issue 153
     CUDF_CUDA_TRY(cudaMemsetAsync((void *)dst, value, count, 0));
   }
   CATCH_STD(env, );
