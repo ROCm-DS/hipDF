e940e111e26418c6750a6693a6b0b97f6bee259d
Philipp Samfass <philipp.samfass@amd.com>
2024-09-11 03:41:18 -0700

    feat(cpp/cmake/thirdparty/get_jitify.cmake):
    use rapids-cmake's new rapids_cpm_jitify_rocm to configure jitify dependency

--- cpp/cmake/thirdparty/get_jitify.cmake
+++ cpp/cmake/thirdparty/get_jitify.cmake
@@ -38,13 +38,8 @@
 
 # This function finds Jitify and sets any additional necessary environment variables.
 function(find_and_configure_jitify)
-  rapids_cpm_find(
-    jitify 2.0.0
-    GIT_REPOSITORY https://github.com/ROCm/jitify
-    GIT_TAG main
-    GIT_SHALLOW TRUE
-    DOWNLOAD_ONLY TRUE
-  )
+  include(${rapids-cmake-dir}/cpm/jitify-rocm.cmake)
+  rapids_cpm_jitify_rocm(DOWNLOAD_ONLY TRUE)
   set(JITIFY_INCLUDE_DIR
       "${jitify_SOURCE_DIR}"
       PARENT_SCOPE
