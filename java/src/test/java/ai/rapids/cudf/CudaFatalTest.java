81d626f7aa393a82f8d744ef20c295b5d2c89469
Mohammad Norouzi <114993067+monoatamd@users.noreply.github.com>
2024-10-21 15:17:01 +0200

    Disable Java CudaFatalTest

--- java/src/test/java/ai/rapids/cudf/CudaFatalTest.java
+++ java/src/test/java/ai/rapids/cudf/CudaFatalTest.java
@@ -14,54 +14,66 @@
  * limitations under the License.
  */
 
-package ai.rapids.cudf;
+// NOTE(HIP/AMD): This test is intended to fail:
+// It is testing how the system handles CUDA-related fatal errors 
+// when working with column vectors in cuDF. It creates a ColumnVector,
+// and later, it tries converting the vector to long integers (asLongs()), 
+// which could trigger CUDA operations.
+// Class BadDeviceBuffer represents a defective device buffer;
+// it causes CUDA fatal error by operating on a bad device buffer.
+// In essence, it is similar to libcudf error_handling_test:
+// (cudf-rocm/cpp/tests/error/error_handling_test.cu).
+// On CUDA, an exception is raised which is handled later in the test.
+// On HIP side, unlike CUDA, execution is not continued after the error on the device.
 
-import org.junit.jupiter.api.Test;
+// package ai.rapids.cudf;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+// import org.junit.jupiter.api.Test;
 
-public class CudaFatalTest {
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertThrows;
 
-  @Test
-  public void testCudaFatalException() {
-    try (ColumnVector cv = ColumnVector.fromInts(1, 2, 3, 4, 5)) {
+// public class CudaFatalTest {
 
-      try (ColumnView badCv = ColumnView.fromDeviceBuffer(new BadDeviceBuffer(), 0, DType.INT8, 256);
-           ColumnView ret = badCv.sub(badCv);
-           HostColumnVector hcv = ret.copyToHost()) {
-      } catch (CudaException ignored) {
-      }
+//   @Test
+//   public void testCudaFatalException() {
+//     try (ColumnVector cv = ColumnVector.fromInts(1, 2, 3, 4, 5)) {
 
-      // CUDA API invoked by libcudf failed because of previous unrecoverable fatal error
-      assertThrows(CudaFatalException.class, () -> {
-        try (ColumnVector cv2 = cv.asLongs()) {
-        } catch (CudaFatalException ex) {
-          assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
-          throw ex;
-        }
-      });
-    }
+//       try (ColumnView badCv = ColumnView.fromDeviceBuffer(new BadDeviceBuffer(), 0, DType.INT8, 256);
+//            ColumnView ret = badCv.sub(badCv);
+//            HostColumnVector hcv = ret.copyToHost()) {
+//       } catch (CudaException ignored) {
+//       }
 
-    // CUDA API invoked by RMM failed because of previous unrecoverable fatal error
-    assertThrows(CudaFatalException.class, () -> {
-      try (ColumnVector cv = ColumnVector.fromBoxedInts(1, 2, 3, 4, 5)) {
-      } catch (CudaFatalException ex) {
-        assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
-        throw ex;
-      }
-    });
-  }
+//       // CUDA API invoked by libcudf failed because of previous unrecoverable fatal error
+//       assertThrows(CudaFatalException.class, () -> {
+//         try (ColumnVector cv2 = cv.asLongs()) {
+//         } catch (CudaFatalException ex) {
+//           assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
+//           throw ex;
+//         }
+//       });
+//     }
 
-  private static class BadDeviceBuffer extends BaseDeviceMemoryBuffer {
-    public BadDeviceBuffer() {
-      super(256L, 256L, (MemoryBufferCleaner) null);
-    }
+//     // CUDA API invoked by RMM failed because of previous unrecoverable fatal error
+//     assertThrows(CudaFatalException.class, () -> {
+//       try (ColumnVector cv = ColumnVector.fromBoxedInts(1, 2, 3, 4, 5)) {
+//       } catch (CudaFatalException ex) {
+//         assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
+//         throw ex;
+//       }
+//     });
+//   }
 
-    @Override
-    public MemoryBuffer slice(long offset, long len) {
-      return null;
-    }
-  }
+//   private static class BadDeviceBuffer extends BaseDeviceMemoryBuffer {
+//     public BadDeviceBuffer() {
+//       super(256L, 256L, (MemoryBufferCleaner) null);
+//     }
 
-}
+//     @Override
+//     public MemoryBuffer slice(long offset, long len) {
+//       return null;
+//     }
+//   }
+
+// }
