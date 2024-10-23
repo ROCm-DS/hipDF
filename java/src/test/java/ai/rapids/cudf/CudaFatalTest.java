012a1495d5a09c659ecbab27e39bda3768301be6
monoatamd <mnorouzi@amd.com>
2024-10-23 03:05:15 -0700

    Use disabled keyword instead of commenting the whole file

--- java/src/test/java/ai/rapids/cudf/CudaFatalTest.java
+++ java/src/test/java/ai/rapids/cudf/CudaFatalTest.java
@@ -26,54 +26,54 @@
 // On CUDA, an exception is raised which is handled later in the test.
 // On HIP side, unlike CUDA, execution is not continued after the error on the device.
 
-// package ai.rapids.cudf;
+package ai.rapids.cudf;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertThrows;
+public class CudaFatalTest {
 
-// public class CudaFatalTest {
+  @Test @Disabled
+  public void testCudaFatalException() {
+    try (ColumnVector cv = ColumnVector.fromInts(1, 2, 3, 4, 5)) {
 
-//   @Test
-//   public void testCudaFatalException() {
-//     try (ColumnVector cv = ColumnVector.fromInts(1, 2, 3, 4, 5)) {
+      try (ColumnView badCv = ColumnView.fromDeviceBuffer(new BadDeviceBuffer(), 0, DType.INT8, 256);
+           ColumnView ret = badCv.sub(badCv);
+           HostColumnVector hcv = ret.copyToHost()) {
+      } catch (CudaException ignored) {
+      }
 
-//       try (ColumnView badCv = ColumnView.fromDeviceBuffer(new BadDeviceBuffer(), 0, DType.INT8, 256);
-//            ColumnView ret = badCv.sub(badCv);
-//            HostColumnVector hcv = ret.copyToHost()) {
-//       } catch (CudaException ignored) {
-//       }
+      // CUDA API invoked by libcudf failed because of previous unrecoverable fatal error
+      assertThrows(CudaFatalException.class, () -> {
+        try (ColumnVector cv2 = cv.asLongs()) {
+        } catch (CudaFatalException ex) {
+          assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
+          throw ex;
+        }
+      });
+    }
 
-//       // CUDA API invoked by libcudf failed because of previous unrecoverable fatal error
-//       assertThrows(CudaFatalException.class, () -> {
-//         try (ColumnVector cv2 = cv.asLongs()) {
-//         } catch (CudaFatalException ex) {
-//           assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
-//           throw ex;
-//         }
-//       });
-//     }
+    // CUDA API invoked by RMM failed because of previous unrecoverable fatal error
+    assertThrows(CudaFatalException.class, () -> {
+      try (ColumnVector cv = ColumnVector.fromBoxedInts(1, 2, 3, 4, 5)) {
+      } catch (CudaFatalException ex) {
+        assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
+        throw ex;
+      }
+    });
+  }
 
-//     // CUDA API invoked by RMM failed because of previous unrecoverable fatal error
-//     assertThrows(CudaFatalException.class, () -> {
-//       try (ColumnVector cv = ColumnVector.fromBoxedInts(1, 2, 3, 4, 5)) {
-//       } catch (CudaFatalException ex) {
-//         assertEquals(CudaException.CudaError.cudaErrorIllegalAddress, ex.getCudaError());
-//         throw ex;
-//       }
-//     });
-//   }
+  private static class BadDeviceBuffer extends BaseDeviceMemoryBuffer {
+    public BadDeviceBuffer() {
+      super(256L, 256L, (MemoryBufferCleaner) null);
+    }
 
-//   private static class BadDeviceBuffer extends BaseDeviceMemoryBuffer {
-//     public BadDeviceBuffer() {
-//       super(256L, 256L, (MemoryBufferCleaner) null);
-//     }
+    @Override
+    public MemoryBuffer slice(long offset, long len) {
+      return null;
+    }
+  }
 
-//     @Override
-//     public MemoryBuffer slice(long offset, long len) {
-//       return null;
-//     }
-//   }
-
-// }
+}
