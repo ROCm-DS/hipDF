5229998359dc0b756d0e706041a25096ad853640
Philipp Samfass <philipp.samfass@amd.com>
2025-02-12 03:10:08 -0800

    fix(udf tests, LLVM IR): do not fix target datalayout.

--- cpp/tests/rolling/grouped_rolling_test.cpp
+++ cpp/tests/rolling/grouped_rolling_test.cpp
@@ -140,9 +140,6 @@ const std::string ptx_func{
 // On CUDA, the index into in_col is computed as start + i, with NUMBA, the index is just i.
 std::string amd_llvm_ir_func{
     R"'''(
-source_filename = "device_func.hip"
-target datalayout = "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7"
-target triple = "amdgcn-amd-amdhsa"
 
 ; Function Attrs: convergent mustprogress noreturn nounwind
 define weak void @__cxa_pure_virtual() #0 {
