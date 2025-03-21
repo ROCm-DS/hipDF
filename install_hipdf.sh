587a545a4ebef120b5fbd90452d377c05aa00f3a
Philipp Samfass <philipp.samfass@amd.com>
2025-03-21 02:30:59 -0700

    fix: relax cupy version and use amd-cupy package name

--- install_hipdf.sh
+++ install_hipdf.sh
@@ -179,7 +179,7 @@ conda activate cupy_dev
   export CUPY_INSTALL_USE_HIP=1
   export ROCM_HOME=/opt/rocm
   export HCC_AMDGPU_TARGET=${AMDGPU_TARGETS//;/,}
-  python3 setup.py --cupy-package-name amd-cupy-for-rocmds bdist_wheel
+  python3 setup.py --cupy-package-name amd-cupy bdist_wheel
   CUPY_WHEEL=$(find ~+ -type f -name "*cupy*.whl")
 
 # build hipdf
