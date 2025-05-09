f8a42f473e7ddd373d8358dfc7c82531dbf9f9f1
Philipp Samfass <philipp.samfass@amd.com>
2025-05-09 09:27:53 -0700

    fix(JitifyPreprocessKernel.*): Fix incorrect order of keyword and target.

--- cpp/cmake/Modules/JitifyPreprocessKernels.cmake
+++ cpp/cmake/Modules/JitifyPreprocessKernels.cmake
@@ -39,7 +39,7 @@ add_executable(jitify_preprocess "${JITIFY_INCLUDE_DIR}/jitify2_preprocess.cpp")
 
 target_compile_definitions(jitify_preprocess PRIVATE "_FILE_OFFSET_BITS=64")
 rapids_hip_set_runtime(jitify_preprocess USE_STATIC ${GPU_STATIC_RUNTIME})
-target_link_libraries(jitify_preprocess hip::host PUBLIC ${CMAKE_DL_LIBS})
+target_link_libraries(jitify_preprocess PUBLIC hip::host  ${CMAKE_DL_LIBS})
 
 # Take a list of files to JIT-compile and run them through jitify_preprocess.
 function(jit_preprocess_files)
