83b088e46c96930967a7359773f8b5a1d30278d0
Philipp Samfass <philipp.samfass@amd.com>
2025-05-13 05:18:19 -0700

    WAR (murmurhash3_x86_32.cuh): disable constexpr (issue #254)
    Context: A compiler error happens due to subsequent specializations not
    using constexpr while the first generic definition of the () operator
    uses constexpr. This seems to be a clang vs GCC issue where clang
    misbehaves.

--- cpp/include/cudf/hashing/detail/murmurhash3_x86_32.cuh
+++ cpp/include/cudf/hashing/detail/murmurhash3_x86_32.cuh
@@ -14,6 +14,29 @@
  * limitations under the License.
  */
 
+
+// MIT License
+//
+// Modifications Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <cudf/fixed_point/fixed_point.hpp>
@@ -38,7 +61,8 @@ struct MurmurHash3_x86_32 {
   {
   }
 
-  __device__ constexpr result_type operator()(Key const& key) const { return this->_impl(key); }
+  // FIXME(HIP/AMD): Cannot use constexpr here due to internal issue 254
+  __device__ /*constexpr*/ result_type operator()(Key const& key) const { return this->_impl(key); }
 
   __device__ constexpr result_type compute_bytes(cuda::std::byte const* bytes,
                                                  std::uint64_t size) const
