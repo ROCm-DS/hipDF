071521dc93b8b286a3d66d71b086f9ca86788d88
Philipp Samfass <philipp.samfass@amd.com>
2025-05-22 02:39:16 -0700

    fix(**/reader_impl_chunking.cu): move __device__  specifier to right position

--- cpp/src/io/parquet/reader_impl_chunking.cu
+++ cpp/src/io/parquet/reader_impl_chunking.cu
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "compact_protocol_reader.hpp"
 #include "io/comp/comp.hpp"
 #include "io/comp/gpuinflate.hpp"
@@ -159,8 +181,8 @@ void print_cumulative_row_info(host_span<cumulative_page_info const> sizes,
  * @brief Functor which reduces two cumulative_page_info structs of the same key.
  */
 struct cumulative_page_sum {
-  cumulative_page_info operator()
-    __device__(cumulative_page_info const& a, cumulative_page_info const& b) const
+   __device__ cumulative_page_info operator()
+   (cumulative_page_info const& a, cumulative_page_info const& b) const
   {
     return cumulative_page_info{0, a.size_bytes + b.size_bytes, a.key};
   }
