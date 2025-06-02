8b5dafc2e6f061496cfcd02d108faaf9aac74f6b
Philipp Samfass <philipp.samfass@amd.com>
2025-06-02 06:17:25 -0700

    refactor(python/*.../conftest.py): remove dead code

--- python/cudf/cudf/tests/conftest.py
+++ python/cudf/cudf/tests/conftest.py
@@ -1,27 +1,5 @@
 # Copyright (c) 2019-2024, NVIDIA CORPORATION.
 
-# MIT License
-#
-# Modifications Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import itertools
 import os
 import pathlib
@@ -198,14 +176,3 @@ def pytest_runtest_makereport(item, call):
     # Set a report attribute for each phase of a call, which can
     # be "setup", "call", "teardown"
     setattr(item, "report", {rep.when: rep})
-
-
-disabled_test_files = []
-
-def pytest_collection_modifyitems(items):
-    exclude = []
-    for item in items:
-        if item.fspath.basename in disabled_test_files:
-            exclude.append(item)
-    for item in exclude:
-        items.remove(item)
