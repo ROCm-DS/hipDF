c624d1ddfa89f00245135d2265cda12a9b642ae2
Philipp Samfass <philipp.samfass@amd.com>
2025-06-23 02:28:32 -0700

    fix(io/*/output_builder.cuh): Remove superfluous __device__.
    Details:
    
    Constexpr are compiled as __host__ __device__ when compiled with hipcc.
    The __device__ specifier seems to override this behavior so the function
    becomes __device__ only which is not desired and results in build errors.

--- cpp/src/io/utilities/output_builder.cuh
+++ cpp/src/io/utilities/output_builder.cuh
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <cudf/types.hpp>
 #include <cudf/utilities/error.hpp>
 #include <cudf/utilities/memory_resource.hpp>
@@ -59,7 +81,9 @@ class split_device_span {
   {
   }
 
-  [[nodiscard]] __device__ constexpr reference operator[](size_type i) const
+  // NOTE(HIP/AMD): Needs to be __host__ __device__, therefore
+  // __device__ was removed, as the function is then only __device__ on HIP.
+  [[nodiscard]] constexpr reference operator[](size_type i) const
   {
     return i < _head.size() ? _head[i] : _tail[i - _head.size()];
   }
