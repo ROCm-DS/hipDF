9b40f61efb96780ad1ede42902f7df4138c9aa4c
Philipp Samfass <philipp.samfass@amd.com>
2025-06-23 01:59:56 -0700

    fix(scalar.cpp): Instantiate missing copy constructors.

--- cpp/src/scalar/scalar.cpp
+++ cpp/src/scalar/scalar.cpp
@@ -485,11 +485,22 @@ timestamp_scalar<T>::timestamp_scalar(timestamp_scalar<T> const& other,
   template timestamp_scalar<TimestampType>::timestamp_scalar( \
     timestamp_scalar<TimestampType> const& other, \
     rmm::cuda_stream_view stream, \
-    rmm::mr::device_memory_resource* mr);
+    rmm::device_async_resource_ref);
 
 #define TS_CTOR(TimestampType, DurationType)                  \
   template timestamp_scalar<TimestampType>::timestamp_scalar( \
     DurationType const&, bool, rmm::cuda_stream_view, rmm::device_async_resource_ref);
+
+// TODO(HIP/AMD): If we removed the following 5 COPY_CTORs, we get an error in TYPED_TEST(TypedScalarTest, CopyConstructor):
+// undefined symbol timestamp_scalar referenced in scalar_test.cpp
+/**
+ * @brief Explicit instantiations of the copy construtor.
+ */
+COPY_CTOR(timestamp_D)
+COPY_CTOR(timestamp_s)
+COPY_CTOR(timestamp_ms)
+COPY_CTOR(timestamp_us)
+COPY_CTOR(timestamp_ns)
 
 /**
  * @brief These are the valid combinations of duration types to timestamp types.
