03381fba64b9d1de2bedde9697229b77c724039d
domcharrier <docharri@amd.com>
2025-08-01 13:14:44 +0000

    hip(performance_tracking): + noop stub for nvtx

--- python/cudf/cudf/utils/performance_tracking.py
+++ python/cudf/cudf/utils/performance_tracking.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
 import contextlib
@@ -7,7 +29,38 @@ import functools
 import hashlib
 import sys
 
-import nvtx
+# import nvtx  # NOTE(HIP/AMD): we follow cudf/pandas/annotation.py here
+
+class nvtx:  # type: ignore
+    """Noop-stub with the same API as nvtx."""
+
+    def enabled():
+        return False
+
+    push_range = lambda *args, **kwargs: None  # noqa: E731
+    pop_range = lambda *args, **kwargs: None  # noqa: E731
+
+    class annotate:
+        """No-op annotation/context-manager"""
+
+        def __init__(
+            self,
+            message: str | None = None,
+            color: str | None = None,
+            domain: str | None = None,
+            category: str | int | None = None,
+        ):
+            pass
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *exc):
+            return False
+
+        __call__ = lambda self, fn: fn  # noqa: E731
+
+nvtx_annotate = nvtx.annotate
 
 import rmm.statistics
 
