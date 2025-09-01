df56bed4e98c82b6f6fca826f477bc02124d756e
domcharrier <docharri@amd.com>
2025-09-01 19:37:44 +0000

    test_groupby.py: *agg_decimal: always else branch
    Always execute the else branch of test_groupby_agg_decimal.
    
    NOTE: Assumes ROCm 7.0+.

--- python/cudf/cudf/tests/test_groupby.py
+++ python/cudf/cudf/tests/test_groupby.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2018-2024, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import collections
 import datetime
 import itertools
@@ -1062,7 +1084,7 @@ def test_groupby_agg_decimal(num_groups, nelem_per_group, func):
     )
 
     expect_df = pdf.groupby("idx", sort=True).agg(func)
-    if rmm._cuda.gpu.runtimeGetVersion() < 11000:
+    if False: # NOTE(HIP/AMD): if-branch is CUDA<11 specific workaround
         with pytest.raises(RuntimeError):
             got_df = gdf.groupby("idx", sort=True).agg(func)
     else:
