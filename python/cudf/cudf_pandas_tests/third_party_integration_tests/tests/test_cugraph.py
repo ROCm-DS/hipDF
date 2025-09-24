2ab61fa8d283a0d791ff3512506bc23f25c6fbec
Philipp Samfass <philipp.samfass@amd.com>
2025-09-24 02:44:43 -0700

    Disable unsupported thirdparty integration tests on HIP/AMD.

--- python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_cugraph.py
+++ python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_cugraph.py
@@ -1,10 +1,40 @@
 # Copyright (c) 2023-2024, NVIDIA CORPORATION.
+
+# MIT License
+#
+# Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import cudf
+import pytest
+# Skip the entire file if running on the HIP AMD port
+if getattr(cudf, "__is_hip_amd_port__", False):
+    pytest.skip("This test is CUDA-specific and not supported on HIP/AMD platform.",
+                allow_module_level=True)
+
 import cugraph
 import cupy as cp
 import networkx as nx
 import numpy as np
 import pandas as pd
-import pytest
+
 
 cugraph_algos = [
     "betweenness_centrality",
